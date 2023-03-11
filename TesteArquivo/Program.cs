9d2c578151a5e67be9cde5ce7332f04d4ffc9fab
Gabriel <gabrielaugustofm@gmail.com>
2023-03-11 10:15:44 -0300

    Adicionar arquivos de projeto.

---
+++ TesteArquivo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+var directories = listaArquivosEPastasNoCaminho(@"D:\");
+
+imprime(directories);
+
+void imprime<Tipo>(Tipo[] lista)
+{
+    foreach (var item in lista)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+
+string[] listaArquivosEPastasNoCaminho (string caminhoParaListar)
+{
+    var diretoriosNoCaminho = Directory.GetDirectories(caminhoParaListar);
+    var arquivosNoCaminho = Directory.GetFiles(caminhoParaListar);
+    return Concat(diretoriosNoCaminho, arquivosNoCaminho);
+}
+
+Type[] Concat<Type>(Type[] x, Type[] y)
+{
+    var z = new Type[x.Length + y.Length];
+    x.CopyTo(z, 0);
+    y.CopyTo(z, x.Length);
+    return z;
+}
